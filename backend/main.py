0318b8070a3cecfaa8d349912d5666887949374e
Mohit Singh Karki <88034167+Mohitk555@users.noreply.github.com>
2026-04-05 16:52:19 +0530

    feat(backend): add API endpoints for no-ticket

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,7 @@
             from fastapi.middleware.cors import CORSMiddleware
 
             from backend.routers.auth import router as auth_router
-from backend.routers.policy import router as policy_router
 from backend.routers.claims import router as claims_router
-from backend.routers.user import router as user_router
-from backend.routers.notification import router as notification_router
 from backend.routers.document import router as document_router
 
             app = FastAPI(
@@ -26,10 +23,7 @@ from backend.routers.document import router as document_router
             )
 
             app.include_router(auth_router, prefix="/api/v1/auths", tags=["Auth Service"])
-app.include_router(policy_router, prefix="/api/v1/policys", tags=["Policy Service"])
 app.include_router(claims_router, prefix="/api/v1/claimss", tags=["Claims Service"])
-app.include_router(user_router, prefix="/api/v1/users", tags=["User Service"])
-app.include_router(notification_router, prefix="/api/v1/notifications", tags=["Notification Service"])
 app.include_router(document_router, prefix="/api/v1/documents", tags=["Document Service"])
 
 
